91ee8e1262926eff37469f4671ee7ea66a0e4ba3
Ben Cason <bencason9@gmail.com>
2019-08-21 20:10:10 -0400

    server tweaks for npm build

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 const express = require('express');
 const routes = require('./routes');
 const app = express();
+//const mongoose = require('mongoose');
 const PORT = process.env.PORT || 3001;
 
 //middleware
@@ -10,11 +11,13 @@ app.use(express.json());
 
 //production settings
 if (process.env.NODE_ENV === 'production') {
-    app.use(express.static('client'));
-  }
+  app.use(express.static('client/build'));
+}
 
 
 app.use(routes);
+
+//mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost:27017/filmDB");
 
 //spin up backend server
 app.listen(PORT, function() {
